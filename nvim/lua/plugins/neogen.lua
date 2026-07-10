787aca25e7f24008dcb154ccb785d84539af33c7
Damian Luciano Muschamp <muschampdamian@gmail.com>
2026-07-09 21:34:30 -0700

    Added copilot to nvim in addition to other LSP stuff for typescript.
    Added Copilot to nvim, neogen for documentation, and misc LSP stuff for
    typescript support, markdown, and other things.

---
+++ nvim/lua/plugins/neogen.lua
@@ -0,0 +1,8 @@
+return {
+  "danymat/neogen",
+  config = true,
+  -- Assign a keymap to make it effortless
+  keys = {
+    { "<leader>nf", "<cmd>Neogen func<cr>", desc = "Neogen Function" },
+  },
+}
