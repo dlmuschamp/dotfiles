14d79dab15cb3d42e82ef2e91dc8f18249d1ce12
Damian Luciano Muschamp <muschampdamian@gmail.com>
2026-07-14 21:24:57 -0700

    Removed powerNotify.sh (deprecated), and changed nvim theme and misc other changes.

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -11,18 +11,5 @@ return {
         map = "<M-e>", -- Alt-e to wrap the word after the cursor in pairs
       },
     })
-
-    -- Custom rule for Typst Math ($)
-    -- This ensures that when you type $, it gives you $$ and puts you in the middle.
-    local Rule = require("nvim-autopairs.rule")
-    local cond = require("nvim-autopairs.conds")
-
-    autopairs.add_rules({
-      Rule("$", "$", "typst")
-        -- Don't pair if the next character is already a $
-        :with_pair(cond.not_after_regex("$"))
-        -- Don't pair if we are in a code block
-        :with_pair(cond.not_before_regex("```", 3)),
-    })
   end,
 }
