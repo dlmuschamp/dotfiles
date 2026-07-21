097523d89e9506d2443f6d13b16a76f2fa0e0530
Damian Luciano Muschamp <muschampdamian@gmail.com>
2026-07-20 21:49:20 -0700

    Working bluetooth event watcher in epoll_manager.c.
    epoll_manager now accurately fetches the bluetooth device connection
    status and prints it to stdout. epoll_manager currently is only set up
    for watching bluetooth events but the framework is there for other
    events as long as the schema for members and interfaces works.

--- automations/src/epoll_manager.c
+++ automations/src/epoll_manager.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/epoll.h>
 #include <sys/wait.h>
+#include <systemd/sd-bus-protocol.h>
 #include <systemd/sd-bus.h>
 #include <unistd.h>
 
@@ -11,6 +13,7 @@
 // Constants
 const int EPOLL_TIMEOUT = -1;
 const char *BASH_SCRIPT_PATH = "./bin/dummy_bash.sh";
+const char *BLUETOOTH_MATCH_FILTERS = "type='signal',sender='org.bluez'";
 
 // Helper Functions
 void execute_bash_script(const char *script_path) {
@@ -19,25 +22,58 @@ void execute_bash_script(const char *script_path) {
   pid_t pid = fork();
 
   switch (pid) {
-  case -1: // error
-    perror("Failed to fork.");
-    break;
-  case 0: // child process
-    execl(script_path, (char *)NULL);
-    perror("execl failed.");
-    _exit(-1);
-    break;
-  default: { // parent process
-    int child_status;
-    waitpid(pid, &child_status, 0);
-    printf("Child process finished. Going back to sleep.\n");
-    break;
-  }
+    case -1: // error
+      perror("Failed to fork.");
+      break;
+    case 0: // child process
+      execl(script_path, (char *)NULL);
+      perror("execl failed.");
+      _exit(-1);
+      break;
+    default: { // parent process
+      int child_status;
+      waitpid(pid, &child_status, 0);
+      printf("Child process finished. Going back to sleep.\n");
+      break;
+    }
   }
 }
 
 int main() {
-  printf("Starting epoll manager...\n");
+  // We need a pointer to hold the D-Bus connection object.
+  // We initialize it to NULL for safety.
+  sd_bus *bus = NULL;
+
+  // 1. Open the system bus connection
+  int open_bus_status;
+  if ((open_bus_status = sd_bus_open_system(&bus)) < 0) {
+    fprintf(stderr, "Failed to open system bus. Error: %s\n",
+            strerror(-open_bus_status));
+    return -1;
+  }
+
+  // 2. Extract the file descriptor
+  int bus_fd;
+  if ((bus_fd = sd_bus_get_fd(bus)) < 0) {
+    fprintf(stderr, "Failed to return a file descriptor. Error: %s\n",
+            strerror(-bus_fd));
+    sd_bus_unref(bus);
+    return -1;
+  }
+
+  printf("Successfully connected to system bus (FD: %d)\n", bus_fd);
+
+  // Adding Match Rule
+  int add_match_status;
+  if ((add_match_status = sd_bus_add_match(bus, NULL, BLUETOOTH_MATCH_FILTERS,
+                                           NULL, NULL)) < 0) {
+    fprintf(
+        stderr,
+        "Failed to add bluetooth match filters to the system bus. Error: %s\n",
+        strerror(-add_match_status));
+    sd_bus_unref(bus);
+    return -1;
+  }
 
   // 1. Create the epoll instance
   int epoll_fd;
@@ -48,13 +84,13 @@ int main() {
 
   // 2. Configure the event we want to watch
   struct epoll_event event;
-  event.events = EPOLLIN;       // We want to wake up when data comes IN
-  event.data.fd = STDIN_FILENO; // We are watching the standard input (keyboard)
+  event.events = EPOLLIN; // We want to wake up when data comes IN
+  event.data.fd = bus_fd; // We are watching the standard input (keyboard)
 
   // 3. Add STDIN to the epoll watch list
 
-  if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, STDIN_FILENO, &event) == -1) {
-    perror("Failed to add STDIN_FILENO to epoll file descriptor. ");
+  if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, bus_fd, &event) == -1) {
+    perror("Failed to add bus_fd to epoll file descriptor. ");
     return -1;
   }
   // Array to hold the events that get triggered
@@ -73,18 +109,50 @@ int main() {
 
     // Loop through all triggered events (right now, it will only ever be 1)
     for (int i = 0; i < event_count; i++) {
-      if (events[i].data.fd == STDIN_FILENO) {
-        printf("Wake up! You pressed Enter.\n");
+      if (events[i].data.fd == bus_fd) {
 
-        // Read the input buffer to clear it, otherwise epoll will immediately
-        // trigger again on the next loop (Level-Triggered behavior!)
-        char buffer[256];
+        // Processing D-Bus Messages
+        sd_bus_message *msg = NULL;
 
-        if (read(STDIN_FILENO, buffer, sizeof(buffer)) == -1) {
-          perror("Unable to read STDIN_FILENO into buffer.");
-          continue; // continue because loop can still work for the next buffer
+        while (sd_bus_process(bus, &msg) > 0) {
+          if (!msg) { // ignore if null
+            continue;
+          }
+
+          const char *path = sd_bus_message_get_path(msg);
+          const char *interface = sd_bus_message_get_interface(msg);
+          const char *member = sd_bus_message_get_member(msg);
+
+          if (path && interface && member) {
+            if (strcmp(member, "PropertiesChanged") == 0) { // exact match
+              const char *changed_interface = NULL;
+              sd_bus_message_read(msg, "s", &changed_interface);
+              sd_bus_message_enter_container(msg, SD_BUS_TYPE_ARRAY, "{sv}");
+
+              while (sd_bus_message_enter_container(msg, SD_BUS_TYPE_DICT_ENTRY,
+                                                    "sv") > 0) {
+                const char *key = NULL;
+                sd_bus_message_read(msg, "s", &key);
+
+                if (strcmp(key, "Connected") == 0) {
+                  sd_bus_message_enter_container(msg, SD_BUS_TYPE_VARIANT, "b");
+                  int is_connected = 1;
+                  sd_bus_message_read(msg, "b", &is_connected);
+                  printf("Device connection stage changed to: %d\n",
+                         is_connected);
+                  sd_bus_message_exit_container(msg);
+
+                } else {
+                  sd_bus_message_skip(msg, "v");
+                }
+                sd_bus_message_exit_container(msg);
+              }
+              sd_bus_message_exit_container(msg);
+            }
+          }
+
+          sd_bus_message_unref(msg);
         }
-
         execute_bash_script(BASH_SCRIPT_PATH);
       }
     }
@@ -94,6 +162,8 @@ int main() {
     perror("Unable to close epoll file descriptor.");
     return -1;
   }
+
+  sd_bus_unref(bus);
 
   return 0;
 }
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = true
+vim.opt.cinoptions:append("l1")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.statusline:append("%= %w")
+vim.opt.updatetime = 300 --this line is not included by default
