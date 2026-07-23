d7b42fd37867f6c51eeae9b03108da7f70117d51
Damian Luciano Muschamp <muschampdamian@gmail.com>
2026-07-22 19:26:08 -0700

    Finished Bluetooth headphones automation.
    Fully working bluetooth automation written in C and bash. C uses epoll
    event listeners to detect bluetooth hardware interrupts. It then forks
    and executes the bash automation script as its child process. The
    project handles my Moondrop Edge headphones and bone conduction
    headphones. On initial startup, it will set the volume of my headphones
    to my default preferred volume, sets the eq to my designated eq in
    easyeffects, and then opens spotify on my preferred workspace. On
    disconnect hardware interrupt, it kills spotify, sets the new default
    audio sink to mute, and resets the default eq to my aria preset.

--- automations/src/epoll_manager.c
+++ automations/src/epoll_manager.c
@@ -9,28 +9,55 @@
 // Define Macros
 #define MAX_EVENTS 4
 #define EPOLL_CREATE_FLAG 0
+#define STATE_ARR_SIZE 4
 
 // Constants
 const int EPOLL_TIMEOUT = -1;
-const char *BASH_SCRIPT_PATH = "./bin/dummy_bash.sh";
-const char *BLUETOOTH_MATCH_FILTERS = "type='signal',sender='org.bluez'";
 
-// Helper Functions
-void execute_bash_script(const char *script_path) {
-  printf("Spawning child process to run script...\n");
+// Statics
+static const char *BT_AUTOMATION_SCRIPT_PATH =
+    "./bin/bluetooth_headphones_automation.sh";
+static const char *BLUETOOTH_MATCH_FILTERS = "type='signal',sender='org.bluez'";
+
+// ensure alias and dev_path are inputted EXACTLY as the commands listed below
+struct known_device {
+  const char *alias;     // bluetoothctl devices
+  const char *dev_path;  // busctl tree org.bluez
+  int connection_status; // 0 is off; 1, on. (-1 default)
+};
+
+// device must be in watchlist to trigger automation
+struct known_device device_watchlist[] = {
+    {"MOONDROP EDGE", "/org/bluez/hci0/dev_41_42_32_30_6A_B8", -1},
+    {"onn Bone Conduction", "/org/bluez/hci0/dev_28_04_C6_93_DE_E6", -1}};
+
+/**
+ * @brief executes the device automation bash script. It passes the device path
+ * and connection status as arguments to the bash script.
+ *
+ * @param script_path is a relative to bin from /src
+ * @param device_path see known_device struct
+ * @param device_alias see known_device struct
+ * @param state 0 is disconnected; 1, connected. (-1 by default/)
+ */
+void exec_dev_automation_bash(const char *script_path, const char *device_path,
+                              const char *device_alias, int state) {
+  char cur_dev_state[STATE_ARR_SIZE];
+  snprintf(cur_dev_state, sizeof(cur_dev_state), "%d", state);
 
   pid_t pid = fork();
 
   switch (pid) {
-    case -1: // error
+    case -1:
       perror("Failed to fork.");
       break;
-    case 0: // child process
-      execl(script_path, (char *)NULL);
+    case 0:
+      execl("/bin/bash", "bash", script_path, device_path, device_alias,
+            cur_dev_state, (char *)NULL);
       perror("execl failed.");
       _exit(-1);
       break;
-    default: { // parent process
+    default: {
       int child_status;
       waitpid(pid, &child_status, 0);
       printf("Child process finished. Going back to sleep.\n");
@@ -85,18 +112,18 @@ int main() {
   // 2. Configure the event we want to watch
   struct epoll_event event;
   event.events = EPOLLIN; // We want to wake up when data comes IN
-  event.data.fd = bus_fd; // We are watching the standard input (keyboard)
+  event.data.fd = bus_fd; // We are watching the D-Bus file descriptor
 
   // 3. Add STDIN to the epoll watch list
-
   if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, bus_fd, &event) == -1) {
     perror("Failed to add bus_fd to epoll file descriptor. ");
     return -1;
   }
+
   // Array to hold the events that get triggered
   struct epoll_event events[MAX_EVENTS];
 
-  printf("Going to sleep. Press Enter to wake me up!\n");
+  printf("Going to sleep. Waiting for Bluetooth events!\n");
 
   // 4. The Infinite Event Loop
   while (1) {
@@ -124,6 +151,24 @@ int main() {
           const char *member = sd_bus_message_get_member(msg);
 
           if (path && interface && member) {
+
+            int target_device_index = -1;
+            int num_devices =
+                sizeof(device_watchlist) / sizeof(device_watchlist[0]);
+
+            for (int i = 0; i < num_devices; i++) {
+              if (strcmp(path, device_watchlist[i].dev_path) == 0) {
+                target_device_index = i;
+                break;
+              }
+            }
+
+            // failed to find a matching path, skip parsing
+            if (target_device_index == -1) {
+              sd_bus_message_unref(msg);
+              continue;
+            }
+
             if (strcmp(member, "PropertiesChanged") == 0) { // exact match
               const char *changed_interface = NULL;
               sd_bus_message_read(msg, "s", &changed_interface);
@@ -136,24 +181,33 @@ int main() {
 
                 if (strcmp(key, "Connected") == 0) {
                   sd_bus_message_enter_container(msg, SD_BUS_TYPE_VARIANT, "b");
-                  int is_connected = 1;
+                  int is_connected = -1;
                   sd_bus_message_read(msg, "b", &is_connected);
-                  printf("Device connection stage changed to: %d\n",
-                         is_connected);
                   sd_bus_message_exit_container(msg);
 
+                  if (is_connected !=
+                      device_watchlist[target_device_index].connection_status) {
+
+                    printf("%s Connection Status: %d\n",
+                           device_watchlist[target_device_index].alias,
+                           is_connected);
+                    exec_dev_automation_bash(
+                        BT_AUTOMATION_SCRIPT_PATH, path,
+                        device_watchlist[target_device_index].alias,
+                        is_connected);
+                    device_watchlist[target_device_index].connection_status =
+                        is_connected;
+                  }
                 } else {
                   sd_bus_message_skip(msg, "v");
                 }
-                sd_bus_message_exit_container(msg);
+                sd_bus_message_exit_container(msg); // Exit DICT_ENTRY
               }
-              sd_bus_message_exit_container(msg);
+              sd_bus_message_exit_container(msg); // Exit ARRAY
             }
           }
-
           sd_bus_message_unref(msg);
         }
-        execute_bash_script(BASH_SCRIPT_PATH);
       }
     }
   }
